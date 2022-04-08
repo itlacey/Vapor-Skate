39be77af8ad69da50ae6f5c245c98990136747d2
Ike Lacey <ikelacey@gmail.com>
2022-04-07 22:55:17 -0400

    Updated UI elements, added dash

--- scripts/floor.gd
+++ scripts/floor.gd
@@ -1,18 +1,17 @@
 extends StaticBody2D
 onready var rand_floor = $CollisionShape2D
 var rng = RandomNumberGenerator.new()
-signal spawn_new
+signal spawn_new()
 
 func _ready():
 	set_friction(0)
 	rng.randomize()
-	rand_floor.scale.x = rng.randf_range(.3, 1.0)
+	rand_floor.scale.x = rng.randf_range(.1, .65)
+	
 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
-
-func _on_new_spawn_vis_screen_exited():
-	print('on main node')
+func _on_new_spawn_vis_viewport_exited(viewport):
 	emit_signal("spawn_new")
